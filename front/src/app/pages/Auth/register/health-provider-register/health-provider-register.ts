b14acbd75c8b982458360345037fcd5c1a847cc9
Abel <lewheabel16@gmail.com>
2026-04-30 10:45:38 +0100

    HealthProviderRegisterForm: formControl and validation

--- front/src/app/pages/Auth/register/health-provider-register/health-provider-register.ts
+++ front/src/app/pages/Auth/register/health-provider-register/health-provider-register.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { PASSWORD_REGEX } from '../../../../core/constants/PASSWORD_REGEX';
 
 @Component({
     selector: 'app-health-provider-register',
@@ -13,18 +14,38 @@ export class HealthProviderRegister {
     private router = inject(Router);
     errors = signal<string>('');
     showPassword = signal<boolean>(false);
+    serverError = signal<string>('');
 
     registerForm = this.formBuilder.group({
         firstName: ['', Validators.required ],
         lastName: ['', [Validators.required, Validators.max(30)]],
         medicalId: ['', [Validators.required]],
-        speciality: [''],
+        speciality: ['', [Validators.required]],
         organisation: ['', Validators.required],
         email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required]],
+        password: ['', [Validators.required, Validators.pattern(PASSWORD_REGEX)]],
         confirmPassword: ['', [Validators.required] ]
     })
 
+    passwordMatchValidators(password:string, confirmPassword:string){
+        return (control: AbstractControl): ValidationErrors | null =>{
+            const pass = control.get(password);
+            const target = control.get(confirmPassword);
+            if (!pass  || !target)  return null;
+            
+            if (pass.value && target.value && pass.valid !== target.value  ) {
+                target.setErrors({...target.errors, mismatch:true})
+                return {mismatch: true};
+            }
+
+            if (target.hasError('mismatch')) {
+                const {mismatch, ...remaining } = target.errors ?? {};
+                target.setErrors(Object.keys(remaining).length ===0 ? null: remaining );
+            }
+
+            return null;
+        }
+    }
     onSubmit() {
         this.router.navigateByUrl('/success-inscription');
         if (this.registerForm.invalid) {
@@ -35,5 +56,14 @@ export class HealthProviderRegister {
 
     togglePasswordVisibility(){
         this.showPassword.set(!this.showPassword());
+    }
+
+    onErrorClose(){
+        this.serverError.set('');
+    }
+
+    isInvalid(field: string){
+        const control = this.registerForm.get(field);
+        return !!(control?.invalid && control?.touched);
     }
 }
