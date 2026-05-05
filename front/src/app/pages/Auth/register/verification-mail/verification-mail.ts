2ced8f0f4b0f0977d0206a4b61a3086295093550
Abel <lewheabel16@gmail.com>
2026-05-05 10:53:53 +0100

    restart

--- front/src/app/pages/Auth/register/verification-mail/verification-mail.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-verification-mail',
-  imports: [],
-  templateUrl: './verification-mail.html',
-  styleUrl: './verification-mail.css',
-})
-export class VerificationMail {}
--- front/src/app/pages/healthProvider/provider-dashboard/provider-dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-provider-dashboard',
-  imports: [],
-  templateUrl: './provider-dashboard.html',
-  styleUrl: './provider-dashboard.css',
-})
-export class ProviderDashboard {}
