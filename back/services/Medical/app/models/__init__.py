16b7daac11388a48a42c7a23e05489faace0cbdb
Abel <lewheabel16@gmail.com>
2026-06-11 20:57:54 +0100

    clean: ignore venv, pycache, .env + feat: patient/practitioner creation endpoints in Medical service

---
+++ back/services/Medical/app/models/__init__.py
@@ -0,0 +1,5 @@
+from .patient import Patient
+from .emergencyContacts import EmergencyContact
+
+# Liste de tous les modèles pour faciliter l'accès extérieur
+__all__ = ["Patient", "EmergencyContact"]
